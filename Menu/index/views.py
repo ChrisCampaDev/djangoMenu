3fd382c72e66ab5aa934309ee6329cc438da3244
ChrisCampaDev <christiancampa546@gmail.com>
2025-02-19 16:05:29 -0500

    v6 for menu food

--- Menu/index/views.py
+++ Menu/index/views.py
@@ -1,8 +1,13 @@
 # views.py
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView,LogoutView
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from index.modules.products.models import Producto
+from django.core.paginator import Paginator
+
 
 class HomeView(LoginRequiredMixin,TemplateView):
     template_name = 'index.html'
@@ -15,3 +20,10 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     next_page = reverse_lazy('login')
+
+def lista_productos(request):
+    productos_list = Producto.objects.all()
+    paginator = Paginator(productos_list, 8)  # Muestra 8 productos por página
+    page = request.GET.get('page')
+    productos = paginator.get_page(page)
+    return render(request, 'list_food.html', {'productos': productos})
